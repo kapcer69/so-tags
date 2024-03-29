0d9bc065545c867d32624efbd064dcdcab527980
Kacper Gotkowski <kgotkowski3@gmail.com>
2024-03-29 19:55:11 +0100

    Get list of tags

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
+import TagTable from './Tags/TagTable';
 
 const queryClient = new QueryClient();
 
 function App() {
-    return <QueryClientProvider client={queryClient}></QueryClientProvider>;
+    return (
+        <QueryClientProvider client={queryClient}>
+            <TagTable />
+        </QueryClientProvider>
+    );
 }
 
 export default App;
